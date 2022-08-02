63aead5ce442f38db0c57eb8b3b1845eafb8e5d3
drakonha <drakohha@tut.by>
2022-08-02 14:52:09 +0300

    new temp in sity

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
+    appid= '563419681fd350c5d50f3dff8a5fa416'
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+    city='Minsk'
+    res= requests.get(url.format(city))
+    print(res.text)
     return render(request,'weather/index.html')
