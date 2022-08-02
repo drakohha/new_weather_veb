606ee1289aead4c5b40846769162628943d15bb4
drakonha <drakohha@tut.by>
2022-08-02 19:33:13 +0300

    new functional delite posts

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,18 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 import requests
+from .models import City
+from .forms import CityForm
+
 
 def index(request):
-    appid= '563419681fd350c5d50f3dff8a5fa416'
+    appid = '563419681fd350c5d50f3dff8a5fa416'
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
-    city='Minsk'
-    res = requests.get(url.format(city)).json()
+    cities = City.objects.all()
 
-    sity_info={
-        'city': city,
-        'temp': res["main"]["temp"],
-        'icon': res["weather"][0]["icon"]
+    if (request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+    form = CityForm
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city)).json()
+        sity_info = {
+            'city': city.name,
+            'temp': res["main"]["temp"],
+            'icon': res["weather"][0]["icon"]
+        }
+        all_cities.append(sity_info)
+    context = {
+        'all_info': all_cities,
+        'form': form
     }
-    context={
-        'info': sity_info
-    }
-    return render(request,'weather/index.html',context)
+
+    return render(request, 'weather/index.html', context)
+
+
+def delete(request):
+    City.objects.all().delete()
+    return HttpResponseRedirect("/")
