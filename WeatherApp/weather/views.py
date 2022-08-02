bb0590786a7b6230d218efedf105a3f20ce4f905
drakonha <drakohha@tut.by>
2022-08-02 15:20:08 +0300

    new model

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -5,6 +5,14 @@ def index(request):
     appid= '563419681fd350c5d50f3dff8a5fa416'
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
     city='Minsk'
-    res= requests.get(url.format(city))
-    print(res.text)
-    return render(request,'weather/index.html')
+    res = requests.get(url.format(city)).json()
+
+    sity_info={
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"]
+    }
+    context={
+        'info': sity_info
+    }
+    return render(request,'weather/index.html',context)
