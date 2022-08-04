e33023f092558b594a5a43fc9d7aed49245ad3d2
drakonha <drakohha@tut.by>
2022-08-04 15:36:58 +0300

    new jwt avtorizations

--- WeatherApp/weather/urls.py
+++ WeatherApp/weather/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
+from .views import RegisterView, LoginWiev , UserView , Logout
 
 urlpatterns = [
 
     path('', views.index),
-    path('delete/',views.delete ,name="delete")
+    path('delete/',views.delete ,name="delete"),
+    path('register', RegisterView.as_view()),
+    path('login', LoginWiev.as_view()),
+    path('user', UserView.as_view()),
+    path('logout', Logout.as_view()),
+
+
 
 ]
--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -2,6 +2,60 @@ from django.shortcuts import render, HttpResponseRedirect
 import requests
 from .models import City
 from .forms import CityForm
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from .models import User
+from rest_framework.exceptions import AuthenticationFailed
+import jwt , datetime
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer= UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+class LoginWiev(APIView):
+    def post(self,request):
+        email=request.data['email']
+        password=request.data['password']
+
+        user= User.objects.filter(email=email).first()
+        if user is None:
+            raise AuthenticationFailed("Пользователь не найден")
+
+        payload={
+            'id': user.id,
+            'exp':datetime.datetime.utcnow()+datetime.timedelta(minutes=300),
+            'iat':datetime.datetime.utcnow()
+        }
+        token = jwt.encode(payload, 'secret',algorithm='HS256').decode('utf-8')
+        response= Response()
+        response.set_cookie(key='jwt', value=token , httponly=True)
+        response.data={
+            'jwt':token
+        }
+        return response
+
+
+class UserView(APIView):
+    def get(self,request):
+        token = request.COOKIES.get('jwt')
+
+
+        if not token:
+            raise AuthenticationFailed("Не прошедший проверку подлинности")
+        try:
+            payload =jwt.decode(token,'secret', algorithm=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed("Не прошедший проверку подлинности")
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
 
 
 def index(request):
@@ -35,3 +89,13 @@ def index(request):
 def delete(request):
     City.objects.all().delete()
     return HttpResponseRedirect("/")
+
+
+class Logout(APIView):
+    def post(self,request):
+        response=Response()
+        response.delete_cookie('jwt')
+        response.data={
+            'messege':" Успешно"
+        }
+        return  response
